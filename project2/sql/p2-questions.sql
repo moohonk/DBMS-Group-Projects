904ff61090f6724912d2857fd7a74197cbad29fb
David Jones <david.a.jones@ou.edu>
2018-09-30 22:05:05 -0500

    Added PNG for several problems.

--- project2/sql/p2-questions.sql
+++ project2/sql/p2-questions.sql
@@ -18,3 +18,45 @@ FROM Contest_Problems, Problem
 WHERE Contest_Problems.pid = Problem.pid;
 
 /* Problem 2.4 */
+SELECT Scored.login, SUM(Scored.score) AS sum
+FROM Scored
+GROUP BY Scored.login;
+
+/* Problem 2.5 */
+SELECT pname
+FROM Problem
+WHERE max_score=(SELECT MAX(max_score) FROM Problem) 
+AND 
+Problem.aid=(SELECT Author.aid FROM Author WHERE Author.aname='Rachel Moran') 
+
+/* Problem 2.6 */
+SELECT pname, total FROM Problem
+LEFT JOIN
+(
+    SELECT COUNT(login) AS total, pid
+    FROM Scored
+    GROUP BY pid
+) AS Totals
+ON Problem.pid = Totals.pid
+WHERE Totals.total > 2;
+
+/* Problem 2.7 */
+SELECT cname, SUM(compensation)
+FROM Contest_Problems 
+LEFT JOIN Problem ON Contest_Problems.pid = Problem.pid
+LEFT JOIN Author ON Problem.aid = Author.aid
+GROUP BY cname
+
+/* Problem 2.8  */
+SELECT cname, MAX(total)
+FROM (
+    SELECT cname, login, SUM(score) AS total
+	FROM Scored LEFT JOIN Contest_Problems ON Scored.pid = Contest_Problems.pid
+	GROUP BY cname, login
+) AS S
+GROUP BY S.cname
+
+
+/* Problem 2.10 */
+DELETE FROM Student
+WHERE grad_year=2018;
--- project2/sql/p3-ddl.sql
+++ project2/sql/p3-ddl.sql
@@ -2,7 +2,6 @@ CREATE TABLE Trainer(
 	trainer_id INT,
 	name_title VARCHAR(15),
 	first_name VARCHAR(15),
-	name AS CONCAT(name_title, " ", first_name),
 	PRIMARY KEY (trainer_id)
 )
 
@@ -10,11 +9,12 @@ CREATE TABLE Gym(
 	city_name VARCHAR(24),
 	type_specialization VARCHAR(12),
 	badge_name VARCHAR(12),
+    gym_leader_id int,
 	-- Gym leader information,
 	-- merged into gym info because of total,
 	-- 1-to-1 participation between tables
 	FOREIGN KEY (gym_leader_id) REFERENCES Trainer(trainer_id),
-	gym_leader_monologue VARCHAR(255),
+	gym_leader_monologue VARCHAR(1024),
 	PRIMARY KEY (city_name)
 )
 
@@ -23,25 +23,24 @@ CREATE TABLE PokemonSpecies(
 	name VARCHAR(24),
 	type_1 VARCHAR(12),
 	type_2 VARCHAR(12),
+    evolves_from int,
 	FOREIGN KEY (evolves_from) REFERENCES PokemonSpecies(species_number),
 	PRIMARY KEY (species_number)
 )
 
--- separate table because while it's 1-to-many,
--- it does not have total participation
-CREATE TABLE PokemonSpeciesEvolution(
-	FOREIGN KEY (evolves_into) REFERENCES PokemonSpecies(species_number)
-)
-
 CREATE TABLE Pokemon(
+    trainer_id int,
 	FOREIGN KEY (trainer_id) REFERENCES Trainer(trainer_id),
 	nickname VARCHAR(16),
 	level INT,
-	FOREIGN KEY (species_number) REFERENCES pokemon_species(species_number),
+    species_number int,
+	FOREIGN KEY (species_number) REFERENCES PokemonSpecies(species_number),
 	PRIMARY KEY (trainer_id, nickname)
 )
 
 CREATE TABLE GymBattle(
+    city_name VARCHAR(24),
+    trainer_id int,
 	FOREIGN KEY (city_name) REFERENCES Gym(city_name),
 	FOREIGN KEY (trainer_id) REFERENCES Trainer(trainer_id),
 )
