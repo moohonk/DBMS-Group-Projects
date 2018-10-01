8a486eb3c7d9b17c3fc4fc9ccacea5b65ff19a9f
JJ Brown <jack.brown.255@gmail.com>
2018-09-30 19:20:45 -0500

    Combined insertions into one file

--- project2/data/author.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Author VALUES (101, 'Alexander Sforza', 2000)
-INSERT INTO Author VALUES (102, 'Rachel Moran', 1500)
-INSERT INTO Author VALUES (103, 'David Terans', 2000)
-INSERT INTO Author VALUES (104, 'Elizabeth Forster', 2500)
